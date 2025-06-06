0fee0fcd5983be3b72e77e086f99a3514b8b65f6
BRAINCaen <brain.caen@gmail.com>
2025-06-06 13:23:08 +0200

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -3,14 +3,15 @@
  * Fichier: src/js/config.js
  */
 
-// Configuration Firebase (remplacez par vos vraies clés)
+// Configuration Firebase - SYNERGIA
 const FIREBASE_CONFIG = {
-    apiKey: "votre-api-key",
-    authDomain: "synergia-v3.firebaseapp.com",
-    projectId: "synergia-v3",
-    storageBucket: "synergia-v3.appspot.com",
-    messagingSenderId: "123456789",
-    appId: "1:123456789:web:abcdef123456"
+    apiKey: "AIzaSyD7uBuAQaOhZ02owkZEuMKC5Vji6PrB2f8",
+    authDomain: "synergia-app-f27e7.firebaseapp.com",
+    projectId: "synergia-app-f27e7",
+    storageBucket: "synergia-app-f27e7.firebasestorage.app",
+    messagingSenderId: "201912738922",
+    appId: "1:201912738922:web:2fcc1e49293bb632899613",
+    measurementId: "G-EGJ79SCMWX"
 };
 
 // Configuration générale de l'app

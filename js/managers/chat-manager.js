75597dde59d5532c4b5060e8319cf319e1516935
BRAINCaen <brain.caen@gmail.com>
2025-06-10 22:31:33 +0200

    Update chat-manager.js

--- js/managers/chat-manager.js
+++ js/managers/chat-manager.js
@@ -6,7 +6,6 @@ const ROOMS_COLLECTION = "chat-rooms";
 const MESSAGES_COLLECTION = "chat-messages";
 
 export class ChatManager {
-    // Liste des salons
     subscribeToRooms(callback) {
         return onSnapshot(collection(db, ROOMS_COLLECTION), snapshot => {
             const rooms = [];
@@ -14,16 +13,16 @@ export class ChatManager {
             callback(rooms);
         });
     }
-    // Ajouter un salon
     async createRoom(name) {
         await addDoc(collection(db, ROOMS_COLLECTION), { name, createdAt: Date.now() });
     }
-    // Supprimer un salon
+    async editRoom(id, newName) {
+        await setDoc(doc(db, ROOMS_COLLECTION, id), { name: newName, editedAt: Date.now() }, { merge: true });
+    }
     async deleteRoom(id) {
         await deleteDoc(doc(db, ROOMS_COLLECTION, id));
-        // (Optionnel) tu peux aussi supprimer les messages du salon ici
+        // (optionnel) tu peux aussi supprimer les messages du salon ici
     }
-    // Messages d'un salon
     subscribeToMessages(roomId, callback) {
         const q = query(
             collection(db, MESSAGES_COLLECTION, roomId, "messages"),
@@ -35,7 +34,6 @@ export class ChatManager {
             callback(messages);
         });
     }
-    // Envoyer message dans un salon
     async sendMessage(roomId, msg) {
         await addDoc(collection(db, MESSAGES_COLLECTION, roomId, "messages"), msg);
     }

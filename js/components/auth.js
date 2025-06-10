a7718c8dbb1700b68ad0ea154b1d974d6fe724a9
BRAINCaen <brain.caen@gmail.com>
2025-06-10 14:18:46 +0200

    Update auth.js

--- js/components/auth.js
+++ js/components/auth.js
@@ -1,24 +1,23 @@
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { AuthManager } from "../managers/auth-manager.js";
-
-const auth = getAuth();
-const manager = new AuthManager(auth);
+import { auth } from "../core/firebase-manager.js";
 
 export async function initAuthComponent(containerId) {
     const res = await fetch("js/components/auth.html");
     const html = await res.text();
-    document.getElementById(containerId).innerHTML += html;
+    document.getElementById(containerId).innerHTML = html;
+
+    const manager = new AuthManager(auth);
 
     const form = document.getElementById("auth-form");
     const errorDisplay = document.getElementById("auth-error");
 
     form.addEventListener("submit", async (e) => {
         e.preventDefault();
-        const email = document.getElementById("email").value;
-        const password = document.getElementById("password").value;
+        const email = document.getElementById("email").value.trim();
+        const password = document.getElementById("password").value.trim();
+        errorDisplay.textContent = "";
         try {
             await manager.signIn(email, password);
-            errorDisplay.textContent = "";
         } catch (err) {
             errorDisplay.textContent = err.message;
         }

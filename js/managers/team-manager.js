2cd91b160688164371bc699ab4d0f329366c92ec
BRAINCaen <brain.caen@gmail.com>
2025-06-10 15:55:59 +0200

    Create team-manager.js

---
+++ js/managers/team-manager.js
@@ -0,0 +1,5 @@
+// Pour la démo, le manager ne fait rien (tout est en localStorage via team.js)
+// Quand tu voudras brancher Firebase, c’est ici que tu ajoutes les méthodes CRUD.
+export class TeamManager {
+    // À implémenter plus tard pour Firebase (addMember, removeMember, getMembers, etc.)
+}

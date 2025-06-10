8597dd1d3aa1bb73d952dd8ba92bff447373ed7e
BRAINCaen <brain.caen@gmail.com>
2025-06-10 22:43:03 +0200

    Update badging-manager.js

--- js/managers/badging-manager.js
+++ js/managers/badging-manager.js
@@ -1,26 +1,52 @@
-import { getFirestore, collection, addDoc, deleteDoc, doc, onSnapshot } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getFirestore, collection, addDoc, onSnapshot, query, orderBy, where, setDoc, doc, getDocs, deleteDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { app } from "../core/firebase-manager.js";
 
 const db = getFirestore(app);
-const BADGING_COLLECTION = "badging";
 
 export class BadgingManager {
-    subscribeToBadges(callback) {
-        const q = collection(db, BADGING_COLLECTION);
-        return onSnapshot(q, snapshot => {
-            const badges = [];
-            snapshot.forEach(doc => {
-                badges.push({ id: doc.id, ...doc.data() });
-            });
-            callback(badges);
+    // Types de pointages (admin)
+    subscribeToTypes(callback) {
+        return onSnapshot(collection(db, "pointage-types"), snapshot => {
+            const types = [];
+            snapshot.forEach(doc => types.push({ id: doc.id, ...doc.data() }));
+            callback(types);
         });
     }
-
-    async addBadge(badge) {
-        await addDoc(collection(db, BADGING_COLLECTION), badge);
+    async addType(type) {
+        await addDoc(collection(db, "pointage-types"), type);
+    }
+    async editType(id, type) {
+        await setDoc(doc(db, "pointage-types", id), type, { merge: true });
+    }
+    async deleteType(id) {
+        await deleteDoc(doc(db, "pointage-types", id));
     }
 
-    async deleteBadge(id) {
-        await deleteDoc(doc(db, BADGING_COLLECTION, id));
+    // Pointages (presences)
+    subscribeToUserPresences(email, callback) {
+        const q = query(collection(db, "presences"), where("user", "==", email), orderBy("timestamp", "desc"));
+        return onSnapshot(q, snapshot => {
+            const pres = [];
+            snapshot.forEach(doc => pres.push({ id: doc.id, ...doc.data() }));
+            callback(pres);
+        });
+    }
+    subscribeToAllPresences(callback) {
+        // Pour admin
+        const q = query(collection(db, "presences"), orderBy("timestamp", "desc"));
+        return onSnapshot(q, snapshot => {
+            const pres = [];
+            snapshot.forEach(doc => pres.push({ id: doc.id, ...doc.data() }));
+            callback(pres);
+        });
+    }
+    async addPresence(data) {
+        await addDoc(collection(db, "presences"), data);
+    }
+    async validatePresence(id, validated=true) {
+        await setDoc(doc(db, "presences", id), { validated }, { merge: true });
+    }
+    async deletePresence(id) {
+        await deleteDoc(doc(db, "presences", id));
     }
 }

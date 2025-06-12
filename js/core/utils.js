2f8950b546c4e44603388697bfe92999ea9f3396
BRAINCaen <brain.caen@gmail.com>
2025-06-12 18:21:10 +0200

    Add files via upload

---
+++ js/core/utils.js
@@ -0,0 +1,25 @@
+
+export function delayUntilElementExists(selector, timeout = 3000) {
+  return new Promise((resolve, reject) => {
+    const interval = setInterval(() => {
+      if (document.querySelector(selector)) {
+        clearInterval(interval);
+        resolve();
+      }
+    }, 100);
+    setTimeout(() => {
+      clearInterval(interval);
+      reject(new Error(`Timeout: l’élément ${selector} n’a pas été trouvé`));
+    }, timeout);
+  });
+}
+
+export function showToast(message) {
+  const toast = document.getElementById("toast");
+  if (!toast) return;
+  toast.innerText = message;
+  toast.style.display = "block";
+  setTimeout(() => {
+    toast.style.display = "none";
+  }, 3000);
+}

b4f6e75c5ec196d1fbb0244cb588a8047eb2ea39
BRAINCaen <brain.caen@gmail.com>
2025-06-13 14:54:23 +0200

    Update AuthService.ts

--- src/modules/auth/AuthService.ts
+++ src/modules/auth/AuthService.ts
@@ -3,12 +3,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyD7uBuAQaOhZ02owkZEuMKC5Vji6PrB2f8",
+  authDomain: "synergia-app-f27e7.firebaseapp.com",
+  projectId: "synergia-app-f27e7",
+  storageBucket: "synergia-app-f27e7.firebasestorage.app",
+  messagingSenderId: "201912738922",
+  appId: "1:201912738922:web:2fcc1e49293bb632899613",
+  measurementId: "G-EGJ79SCMWX"
 };
 
 const app = initializeApp(firebaseConfig);

1cf25155f055d14df64a11a3c3a7274ccd17a15d
BRAINCaen <brain.caen@gmail.com>
2025-06-13 15:58:14 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,7 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './shared/components/Login';
-import Register from './shared/components/Register';
-import Timetracking from './shared/components/Timetracking';
-import TimetrackingAdmin from './shared/components/TimetrackingAdmin';
-
-const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/pointage" element={<Timetracking />} />
-      <Route path="/admin" element={<TimetrackingAdmin />} />
-    </Routes>
-  </BrowserRouter>
-);
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>

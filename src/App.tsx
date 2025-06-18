f5ef5f24a52c6d0e78edab53a1e54af2ae906373
BRAINCaen <brain.caen@gmail.com>
2025-06-18 16:57:00 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,6 @@ import Login from "./shared/components/Login";
 
 function App() {
   return (
-    <pre style={{ color: "#222", background: "#eee", padding: 8, borderRadius: 4, fontSize: 12 }}>
-  {JSON.stringify(import.meta.env, null, 2)}
-</pre>
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-gray-900 to-gray-700">
       <div className="bg-white p-8 rounded-lg shadow-lg">
         <h1 className="text-3xl font-bold text-center text-blue-700 mb-4">
@@ -13,6 +10,9 @@ function App() {
         </h1>
         <p className="text-center">Bienvenue sur votre nouvelle application !</p>
         <Login />
+        <pre style={{ color: "#222", background: "#eee", padding: 8, borderRadius: 4, fontSize: 12 }}>
+          {JSON.stringify(import.meta.env, null, 2)}
+        </pre>
       </div>
     </div>
   );
